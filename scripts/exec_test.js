2d227b30ef4c94e6d922667f0c0edd7722fbafe0
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2022-05-28 13:32:43 +0700

    feat(scripts): show error message when test failed
    Signed-off-by: Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>

--- scripts/exec_test.js
+++ scripts/exec_test.js
@@ -1,6 +1,7 @@
 import fs from 'node:fs';
 import process from 'node:process';
 import path from 'node:path';
+import {format} from 'node:util';
 
 const files = await fs.promises.readdir(
 	path.resolve(process.cwd(), 'submissions'),
@@ -9,5 +10,10 @@ const files = await fs.promises.readdir(
 files
 	.filter((fl) => /.*(spec|test)\.((m|c)?js)/gi.test(fl))
 	.forEach(async (file) => {
-		import(path.resolve(process.cwd(), 'submissions', file));
+		import(path.resolve(process.cwd(), 'submissions', file)).catch((e) => {
+                  console.error(file, 'test failed because', format(
+                    "'%s'", e.message));
+                }).then(() => {
+                  console.log(file, 'test success');
+                });
 	});
