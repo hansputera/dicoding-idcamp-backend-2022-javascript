e7b4c5179a51e42986fc92cc7f1a2ff936d4c84a
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2022-05-28 13:53:59 +0700

    fix(scripts): don't log unsuccessful test as 'test success'
    Signed-off-by: Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>

--- scripts/exec_test.js
+++ scripts/exec_test.js
@@ -1,7 +1,7 @@
 import fs from 'node:fs';
 import process from 'node:process';
 import path from 'node:path';
-import {format} from 'node:util';
+import { format } from 'node:util';
 
 const files = await fs.promises.readdir(
 	path.resolve(process.cwd(), 'submissions'),
@@ -10,10 +10,13 @@ const files = await fs.promises.readdir(
 files
 	.filter((fl) => /.*(spec|test)\.((m|c)?js)/gi.test(fl))
 	.forEach(async (file) => {
-		import(path.resolve(process.cwd(), 'submissions', file)).catch((e) => {
-                  console.error(file, 'test failed because', format(
-                    "'%s'", e.message));
-                }).then(() => {
-                  console.log(file, 'test success');
-                });
+		let success = true;
+		import(path.resolve(process.cwd(), 'submissions', file))
+			.catch((e) => {
+				success = false;
+				console.error(file, 'test failed because', format("'%s'", e.message));
+			})
+			.then(() => {
+				if (success) console.log(file, 'test success');
+			});
 	});
--- submissions/operator_if_else.test.js
+++ submissions/operator_if_else.test.js
@@ -1,8 +1,21 @@
-import * as scoreChecker from './operator_if_else.js';
+import { scoreChecker } from './operator_if_else.js';
 import assert from 'node:assert';
 
 const randomNilai = Math.floor(Math.random() * 100);
 const result = scoreChecker(randomNilai);
 
+console.log(`[OPERATOR_IF_ELSE]:: [RESULT= '${result}']::[NILAI= '${randomNilai}']`);
+
 assert.strictEqual(typeof result, 'string');
-console.log('Nilai:', randomNilai.toString().concat('\n'), result);
+
+if (randomNilai >= 90) {
+	assert.strictEqual(result, 'Selamat! Anda mendapatkan nilai A.');
+} else if (randomNilai >= 80 && randomNilai <= 89) {
+	assert.strictEqual(result, 'Anda mendapatkan nilai B.');
+} else if (randomNilai >= 70 && randomNilai <= 79) {
+	assert.strictEqual(result, 'Anda mendapatkan nilai C.');
+} else if (randomNilai >= 60 && randomNilai <= 69) {
+	assert.strictEqual(result, 'Anda mendapatkan nilai D.');
+} else {
+	assert.strictEqual(result, 'Anda mendapatkan nilai E.');
+}
