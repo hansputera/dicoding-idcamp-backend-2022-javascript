94786602d05e27adb337b30747cabe1b253f3013
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2022-05-28 12:50:13 +0700

    feat(*): initial commit
    Signed-off-by: Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>

---
+++ submissions/tipe_data.js
@@ -0,0 +1,18 @@
+/**
+ * TODO:
+ * Buatlah variabel firstName, lastName, age, isMarried dengan ketentuan:
+ *  - firstName: bertipe data string, dengan nilai nama depan Anda.
+ *  - lastName: bertipe data string, dengan nilai nama belakang Anda.
+ *  - age: bertipe data number, dengan nilai umur Anda.
+ *  - isMarried: bertipe data boolean, dengan bebas Anda tentukan.
+ */
+
+const firstName = 'Hanif Dwy';
+const lastName = 'Putra S';
+const age = 15;
+const isMarried = false;
+
+module.exports = {
+  firstName, lastName, age, isMarried,
+};
+
